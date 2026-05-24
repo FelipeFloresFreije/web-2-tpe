6d74c2f3dbbda531a5174fc19a666ed9e13f1610
FelipeFloresFreije <125514400+FelipeFloresFreije@users.noreply.github.com>
2026-05-23 23:58:51 -0300

    Add files via upload

---
+++ tp/app/controllers/authController.php
@@ -0,0 +1,66 @@
+<?php
+require_once './app/models/userModel.php';
+require_once './app/views/authView.php';
+
+class AuthController {
+    private $userModel;
+    private $view;
+
+    function __construct() {
+        $this->userModel = new userModel();
+        $this->view = new authView();
+    }
+
+    public function showLogin() {
+        $this->view->showLogin();
+    }
+
+    public function validate($request) {
+        if (empty($_SESSION['USER_ID'])) {
+            header('Location: login');
+            exit();
+        } else {
+            header('Location: home');
+            exit();
+        }
+    }
+
+    public function doLogin($request) {
+        if (empty($_POST['email']) || empty($_POST['password'])) {
+            return $this->view->showLogin("Faltan datos obligatorios", $request->user);
+        }
+
+        $user = $_POST['email'];
+        $password = $_POST['password'];
+
+        $userFromDB = $this->userModel->getByUser($user);
+
+        if($userFromDB && password_verify($password, $userFromDB->password)) {
+            $_SESSION['USER_ID'] = $userFromDB->id_usuario;
+            $_SESSION['USER_NAME'] = $userFromDB->email;
+            $_SESSION['ROLE_USER'] = $userFromDB->rol;
+            header('Location: ' . BASE_URL);
+            die();
+        } else {
+            return $this->view->showLogin("Usuario o contraseña incorrecta", $request->user);
+        }
+    }
+    
+    public function logout() {
+        session_destroy();
+        header('Location: ' . BASE_URL);
+        die();
+    }
+
+    public function registrar($request) {
+        if(empty($_POST['email']) || empty($_POST['password'])) {
+            return $this->view->showRegistrarse("Faltan datos obligatorios", $request->user);
+        }
+        $user = $_POST['email'];
+        $password = password_hash($_POST['password'],PASSWORD_BCRYPT) ;
+
+        $this->userModel->insert($user,$password);
+        header('Location: ' . BASE_URL);
+        die();
+    }
+}
---
+++ tp/router.php
@@ -0,0 +1,120 @@
+<?php
+    session_start();
+    require_once './config.php';
+    require_once '../tp/app/middleware/sessionMiddleware.php';
+    require_once '../tp/app/controllers/authController.php';
+    require_once '../tp/app/controllers/seccionController.php';  
+    require_once './app/controllers/noticiasController.php';
+    require_once './app/middleware/guardMiddleware.php';
+    
+    define('BASE_URL', '//' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']) . '/');
+
+
+    // accion por defecto
+    $action = 'home';
+
+    if (!empty($_GET['action'])) {
+        $action = $_GET['action'];
+    }
+
+    $params = explode('/', $action);
+    $request = new stdClass();
+    $request = (new sessionMiddleware())->run($request);
+    
+    switch($params[0]) {
+            case 'home':
+                //llamo ambos getAll en categorias
+                var_dump($request);
+                $sessionMiddleware = new SessionMiddleware();
+                $request = $sessionMiddleware->run($request);
+                $controller = new SeccionController();
+                $controller->showSeccion($request);
+                break;
+            case 'mostrarSeccion':
+                
+                break;
+            case 'eliminarSeccion':
+                
+                break;
+            case 'agregarSeccion':
+                
+                break;
+            case'modificarSeccion':
+                
+                break;
+            case 'mostrarNoticia':
+                $controller = new NoticiasController();
+                if (!empty($params[1])) {
+                    $controller->show($params[1]);
+                } else {
+                    echo("Ha ingresado mal los parametros");
+                }
+                break;
+
+            case 'eliminarNoticia':
+                $request = (new GuardMiddleware())->run($request);
+                $controller = new NoticiasController();
+                if (!empty($params[1])) {
+                    $id = $params[1];
+                    $controller->delete($id);
+                } else {
+                    echo("Ha ingresado mal los parametros");
+                }
+                break;
+
+            case 'agregarNoticia':
+                $request = (new GuardMiddleware())->run($request);
+                $controller = new NoticiasController();
+                $controller->add();
+                break;
+
+            case 'modificar':
+                $request = (new GuardMiddleware())->run($request);
+                //chequea que este logueado, si no lo manda al login
+                $controller = new NoticiasController();
+                if (!empty($params[1])) {
+                    $id = $params[1];
+                    $controller->editar($id);
+                } else {
+                    echo("Ha ingresado mal los parametros");
+                }
+                break;
+
+            case'modificarNoticia':
+                $request = (new GuardMiddleware())->run($request);
+                //chequea que este logueado, si no lo manda al login
+                if (!empty($params[1])) {
+                    $controller = new SeccionModel();
+                    $secciones = $controller->getAll();
+                    $user = $request->user;
+                    $id_editar = $params[1];
+                    include './templates/form_modificar.phtml';
+                } else {
+                    echo("Ha ingresado mal los parametros");
+                }
+                break;
+
+            case 'login':
+                $controller = new AuthController();
+                $controller->doLogin($request);
+                break;
+
+            case 'validate':
+                $authController = new AuthController();
+                $authController->validate($request);
+                break;
+
+            case 'register':
+                $authController = new AuthController();
+                $authController->registrar($request);
+                break;
+
+            case 'logout':
+                $authController = new AuthController();
+                $authController->logout();
+                break;
+
+            default: 
+                echo "404 Page Not Found";
+    }
+?>
